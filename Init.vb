ebd1e97437bf20481eae921f7181f52ed4301b77
Mr. Clark <geoff@clarktribegames.com>
2021-11-10 19:47:45 -0500

    1.0.000.0002 - Part B
    Adjusted Date Query to read from online DB instead of hardcode.

--- Init.vb
+++ Init.vb
@@ -3,6 +3,7 @@
         Dim VersionParts() As String = Strings.Split((System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()), ".", 4)
         Mem.Version = VersionParts(0) & "." & VersionParts(1) & "." & Toolbox.VersionConverter(VersionParts(2), 3) & "." &
             Toolbox.VersionConverter(VersionParts(3), 4)
+        Mem.UpdaterD = CTGMySQL.CTGMySQL.QueryDate(LCase(Mem.Updater).Replace(".exe", ""))
         If System.IO.File.Exists(Mem.Updater) Then
             If System.IO.File.GetLastWriteTime(Mem.Updater) < Convert.ToDateTime(Mem.UpdaterD) Then
                 System.IO.File.Delete(Mem.Updater)
--- Mem.vb
+++ Mem.vb
@@ -3,7 +3,7 @@
     Public Shared Version As String
     Public Shared Available As String
     Public Shared Updater As String = "CTGUpdater.exe"
-    Public Shared UpdaterD As String = "11/10/2021"
+    Public Shared UpdaterD As String
     Public Shared AppName As String = System.Reflection.Assembly.GetExecutingAssembly().GetName.Name
     Public Shared TheMagic As String = ((FileVersionInfo.
         GetVersionInfo(System.Reflection.Assembly.GetEntryAssembly().Location)).
